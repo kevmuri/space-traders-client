57b577a2a3a2cc32110b139958708a0c22347223
kevmuri <kevmuri@umich.edu>
2023-06-11 21:42:52 -0400

    save progress on agent creation

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,28 @@
-import {getMyAgent} from "@/utils/apiHandler";
+import {getMyAgent, createMyAgent} from "@/utils/apiHandler";
 import {React, useEffect, useState} from "react";
 import {console} from "next/dist/compiled/@edge-runtime/primitives/console";
+import {create} from "axios";
 
 export default function Home() {
   const [agentInfo, setAgentInfo] = useState('');
 
   async function getAgentInfo() {
-      const resp = await getMyAgent();
-      const respData = resp.data['symbol'];
+      let resp = await getMyAgent();
+      const respData = JSON.stringify(resp.data.data);
+      console.log(respData)
       setAgentInfo(respData);
+    }
+
+    async function createAgent() {
+      let resp = await createMyAgent();
+      console.log(resp)
     }
 
   return (
       <div>
-          <button onClick={getAgentInfo}>Button</button>
+          <button onClick={getAgentInfo}>Get Agent Info</button>
           <strong>{agentInfo}</strong>
+          <button onClick={createAgent}>Create Agent</button>
       </div>
   );
 }
--- utils/apiHandler.js
+++ utils/apiHandler.js
@@ -1,14 +1,37 @@
 import {API_URL} from "@/utils/constants";
 import axios from 'axios';
 import {jwt} from "@/utils/jwt";
+import {compareRouterStates} from "next/dist/shared/lib/router/utils/compare-states";
 
-const options = {
-    headers: {
-        Authorization: 'Bearer ' + jwt,
-    },
-};
+function buildJSON(includeAuth) {
+    let jsonObject = {
+        headers: {},
+        data: {},
+    };
+
+    if (includeAuth === true) {
+        jsonObject['headers']['Authorization'] = 'Bearer ' + jwt;
+    }
+
+    console.log(arguments.length)
+    for (let i = 1; i < arguments.length - 1; i++) {
+        console.log(arguments[i])
+        jsonObject[arguments[i][0]][arguments[i][1]] = arguments[i][2];
+    }
+    console.log(jsonObject);
+    return jsonObject;
+}
 
 export async function getMyAgent() {
-    const response = await axios.get(API_URL + '/my/agent', options);
-    return response.data;
+    const response = await axios.get(API_URL + '/my/agent', buildJSON());
+    return response;
+}
+
+export async function createMyAgent() {
+   let jsonValues = (['headers', 'Content-Type', 'application/json'],
+        ['data', 'symbol', 'TESTLOBSTER'],
+        ['data', 'faction', 'COSMIC']);
+
+   const response = await axios.post(API_URL + '/register', buildJSON(true, ...jsonValues));
+   return response;
 }
